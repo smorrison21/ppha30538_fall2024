133c9c43d2823d0bbb34109b93afc015fac9f1eb
smorrison21 <morrisons@uchicago.edu>
2024-10-02 13:47:31 -0500

    Create def say_hello(name):.py

---
+++ def say_hello(name):.py
@@ -0,0 +1,10 @@
+def say_hello(name):
+    print("Hello, " + name)
+
+say_hello("VS Code")
+pip install altair
+from gettext import install
+import altair as alt
+- VSCode: [https://code.visualstudio.com/](https://code.visualstudio.com/)
+- Quarto: [https://quarto.org/docs/get-started/](https://quarto.org/docs/get-started/)
+- Jupyter: `pip install jupyter`
