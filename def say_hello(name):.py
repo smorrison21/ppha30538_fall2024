f92eb947b2943276ef5937ddf1605e542127dd79
smorrison21 <morrisons@uchicago.edu>
2024-10-07 09:19:02 -0500

    changes

--- def say_hello(name):.py
+++ def say_hello(name):.py
@@ -5,6 +5,7 @@ say_hello("VS Code")
 pip install altair
 from gettext import install
 import altair as alt
+
 - VSCode: [https://code.visualstudio.com/](https://code.visualstudio.com/)
 - Quarto: [https://quarto.org/docs/get-started/](https://quarto.org/docs/get-started/)
 - Jupyter: `pip install jupyter`
